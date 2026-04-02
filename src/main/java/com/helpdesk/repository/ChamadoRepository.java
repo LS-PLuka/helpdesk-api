f39e7b624cd3646eb141fe32db64eb08c2cf997a
Pedro Luka <pedrolukagt@gmail.com>
2026-04-02 17:52:30 -0300

    feat: criando ChamadoService e implementando metodos

--- src/main/java/com/helpdesk/repository/ChamadoRepository.java
+++ src/main/java/com/helpdesk/repository/ChamadoRepository.java
@@ -2,6 +2,8 @@ package com.helpdesk.repository;
 
 import com.helpdesk.model.entity.Chamado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
 }
